ee4e8f235bae6e7de64dafb2a8d6f52032a7c2c9
Alker51 <remyrobin51@gmail.com>
2025-03-06 19:08:42 +0100

    AJOUT : Creation de la methode pour creer un utilisateur

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+#[Route('/user', name: 'app_user_')]
 
 final class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/', name: 'home')]
+
     public function index(): Response
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
+        ]);
+    }
+    #[Route('/create', name: 'create')]
+    public function create(Request $request, UserRepository $userRepository): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->save($user, true);
+
+            return $this->redirectToRoute('app_user_home', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('user/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
