56e56b28698ea27aa53331db2866fae9b75f3544
Alker51 <remyrobin51@gmail.com>
2025-03-13 17:08:02 +0100

    MODIF : Ajout du hash du mot de passe pour le stocker dans la base. (Debut suppr de compte avec verif de mdp)

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,6 +8,8 @@ use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 #[Route('/user', name: 'app_user_')]
 
@@ -22,13 +24,20 @@ final class UserController extends AbstractController
         ]);
     }
     #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
-    public function create(Request $request, UserRepository $userRepository): Response
+    public function create(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $user->getPassword()
+            );
+            $user->setPassword($hashedPassword);
+
             $userRepository->save($user, true);
 
             return $this->redirectToRoute('app_user_home', [], Response::HTTP_SEE_OTHER);
@@ -37,5 +46,15 @@ final class UserController extends AbstractController
         return $this->render('user/new.html.twig', [
             'form' => $form,
         ]);
+    }
+
+    #[Route('/delete', name: 'delete', methods: ['GET', 'POST'])]
+    public function delete(Request $request, User $user, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $passwordEntry = '';
+
+        if(!$passwordHasher->isPasswordValid($user, $passwordEntry)) {
+            throw new AccessDeniedHttpException();
+        }
     }
 }
