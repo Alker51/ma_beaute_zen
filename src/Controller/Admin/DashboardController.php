c1549b8c2db2fddcb69dc39b6419445515d76e60
Alker51 <remyrobin51@gmail.com>
2025-03-20 19:12:30 +0100

    EDIT : Mise en page de l'admin + ajout du crud produit

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,12 +8,24 @@ use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
+    private $security;
+    private $doctrine;
+
+    public function __construct(Security $security, ManagerRegistry $doctrine)
+    {
+        $this->security = $security;
+        $this->doctrine = $doctrine;
+    }
     public function index(): Response
     {
 
@@ -53,12 +65,29 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Compte Client', 'fa fa-user', User::class);
         yield MenuItem::linkToCrud('Taxe', 'fa fa-percent', Tax::class);
         yield MenuItem::section();
-        yield MenuItem::linkToUrl('Retour à la page d\'accueil', 'fa-solid fa-right-from-bracket', '/')
-            ->setCssClass('menu-item-return');
+        yield MenuItem::linkToLogout('Logout', 'fa-solid fa-right-from-bracket')->setCssClass('menu-item-return');;
     }
 
     public function configureAssets(): Assets
     {
         return Assets::new()->addCssFile('styles/app.css');
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+    $user = $this->doctrine->getRepository(User::class)->findOneBy(['email' => $user->getUserIdentifier()]);
+        return parent::configureUserMenu($user)
+            // use the given $user object to get the user name
+            ->setName($user->getFirstName() . ' ' . $user->getLastName())
+            // use this method if you don't want to display the name of the user
+            ->displayUserName(true)
+            // you can also pass an email address to use gravatar's service
+            ->setGravatarEmail($user->getEmail())
+
+            // you can use any type of menu item, except submenus
+            ->addMenuItems([
+                MenuItem::section(),
+                MenuItem::linkToLogout('Logout', 'fa fa-sign-out'),
+            ]);
     }
 }
