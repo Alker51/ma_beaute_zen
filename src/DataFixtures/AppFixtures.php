94a0e454b2ad61540eb9ea12f3f86a5a34ae2234
Alker51 <remyrobin51@gmail.com>
2025-04-06 12:09:08 +0200

    AJOUT : création d'un utilisateur administrateur dans AppFixtures Mise en place de l'initialisation d'un utilisateur administrateur avec des détails prédéfinis, incluant un mot de passe haché et le rôle administrateur. Mise à jour de AppFixtures pour utiliser UserPasswordHasherInterface afin de garantir une gestion sécurisée des mots de passe.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,21 +3,54 @@
 namespace App\DataFixtures;
 
 use App\Entity\Gender;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $genders = ['Homme', 'Femme', 'Autre / Ne ce prononce pas'];
+        $genderEntities = [];
 
         foreach ($genders as $gender) {
             $genre = new Gender();
             $genre->setName(ucfirst($gender));
             $manager->persist($genre);
+
+            $genderEntities[$gender] = $genre;
         }
 
+        $manager->flush();
+
+        $admin = new User();
+        $admin->setEmail('admin@example.com'); // Email de l'administrateur
+        $admin->setFirstName('ADMIN');
+        $admin->setLastName('ISTRATOR');
+        $admin->setPhone('0123456789');
+        $admin->setAdress('123 Rue des Admins');
+        $admin->setCity('AdminCity');
+        $admin->setZipcode('12345');
+        $admin->setBirthDate(new \DateTime('1980-01-01')); // Date de naissance
+        $admin->setGender($genderEntities['Homme']); // Genre associé
+
+        // Définir le rôle d'admin
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        // Hachage et définition du mot de passe
+        $hashedPassword = $this->passwordHasher->hashPassword($admin, 'admin123');
+        $admin->setPassword($hashedPassword);
+
+        $manager->persist($admin);
         $manager->flush();
     }
 }
