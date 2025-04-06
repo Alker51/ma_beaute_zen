befea4e6b1fe6a1a4d554f52f022c749a46ef1d1
Alker51 <remyrobin51@gmail.com>
2025-04-06 13:55:17 +0200

    Ajoute un lien vers la gestion des produits dans le Dashboard
    Un nouveau MenuItem a été ajouté pour permettre la gestion des entités "Produit" directement depuis le tableau de bord administrateur. Cela améliore l'accessibilité et facilite la navigation pour les administrateurs.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Produit;
 use App\Entity\Tax;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
@@ -64,6 +65,7 @@ class DashboardController extends AbstractDashboardController
         //yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Compte Client', 'fa fa-user', User::class);
         yield MenuItem::linkToCrud('Taxe', 'fa fa-percent', Tax::class);
+        yield MenuItem::linkToCrud('Produits', 'fa fa-box', Produit::class);
         yield MenuItem::section();
         yield MenuItem::linkToLogout('Logout', 'fa-solid fa-right-from-bracket')->setCssClass('menu-item-return');;
     }
