31d6a44f0d0d29e32b3c0148f6dd6a994e08cd47
Alker51 <remyrobin51@gmail.com>
2025-04-10 18:52:10 +0200

    Ajoute des actions CRUD au ProduitController
    Ajout des méthodes 'add', 'edit' et 'delete' dans ProduitController avec leurs templates associés. Cela permet de gérer les opérations CRUD de base pour l'entité produit.

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -4,14 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\Exception\NoSuchOptionException;
 use Symfony\Component\Routing\Attribute\Route;
-
+#[Route('/produit', name: 'app_produit')]
 final class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'app_produit')]
+    #[Route('/', name: '_index')]
     public function index(): Response
     {
         return $this->render('produit/index.html.twig', [
+            'controller_name' => 'ProduitController',
+        ]);
+    }
+
+    public function add(): Response
+    {
+        return $this->render('produit/add.html.twig', [
+            'controller_name' => 'ProduitController',
+        ]);
+    }
+
+    public function edit(): Response
+    {
+        return $this->render('produit/edit.html.twig', [
+            'controller_name' => 'ProduitController',
+        ]);
+    }
+
+    public function delete(): Response
+    {
+        return $this->render('produit/delete.html.twig', [
             'controller_name' => 'ProduitController',
         ]);
     }
