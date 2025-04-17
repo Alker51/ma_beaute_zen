52ac83fa84bec55fbab88f22cf1e7cc2748fb1df
Alker51 <remyrobin51@gmail.com>
2025-04-17 17:07:15 +0200

    Réorganise les champs de formulaire dans ProduitCrudController
    Réorganisation des sections avec l'ajout de FormField pour mieux structurer les informations (produit, prix, stock, promotion, visuel). Cela améliore la lisibilité et la gestion des champs dans l'interface administrateur. Simplifications et suppressions des duplications liées aux champs taxe et prix TTC.

--- src/Controller/Admin/ProduitCrudController.php
+++ src/Controller/Admin/ProduitCrudController.php
@@ -8,6 +8,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -22,12 +23,34 @@ class ProduitCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
+            FormField::addColumn(4,'Informations du produit'),
             TextField::new('name')->setLabel('Nom du produit'),
             TextEditorField::new('description')->hideOnIndex(),
             BooleanField::new('active')->setLabel('Produit en ligne ?'),
+            NumberField::new('delay')->setLabel('Durée de la préstation (en minutes)'),
+
+            FormField::addColumn(4,'Prix, Stock et Promotion'),
+            FormField::addFieldset('Prix et TVA'),
             NumberField::new('priceHT')->setLabel('Prix HT'),
+            AssociationField::new('taxeId')
+                ->setLabel('TVA appliquée')
+                ->formatValue(function ($value, Produit $entity) {
+                    // Affiche une chaîne lisible
+                    return $entity->getTaxeId()
+                        ? $entity->getTaxeId()->getName() . ' (' . $entity->getTaxeId()->getValue() . '%)'
+                        : 'Aucune taxe';
+                }),
+            NumberField::new('prixTTC', 'Prix TTC')
+                ->setLabel('Prix TTC')
+                ->formatValue(function ($value, Produit $entity) {
+                    return number_format($entity->getPrixTTC(), 2, ',', ' ') . ' €';
+                })
+                ->setFormTypeOption('disabled', true),
+
+            FormField::addFieldset('Promotion'),
             BooleanField::new('promoActive')->setLabel('Promotion en cours ?'),
             NumberField::new('promoPercent')->setLabel('Pourcentage promotionnel (si promo activé.'),
+            FormField::addFieldset('Stock'),
             NumberField::new('stock')->setLabel('Stocks disponibles')
                 ->formatValue(function ($value, $entity) {
                     // Prévisuel HTML des images
@@ -44,21 +67,8 @@ class ProduitCrudController extends AbstractCrudController
                     $html .= '">' . $entity->getStock() . '</span>';
                     return $html;
                 }),
-            AssociationField::new('taxeId')
-                ->setLabel('Taxe appliquée')
-                ->formatValue(function ($value, Produit $entity) {
-                    // Affiche une chaîne lisible
-                    return $entity->getTaxeId()
-                        ? $entity->getTaxeId()->getName() . ' (' . $entity->getTaxeId()->getValue() . '%)'
-                        : 'Aucune taxe';
-                }),
-            NumberField::new('prixTTC', 'Prix TTC')
-                ->setLabel('Prix TTC')
-                ->formatValue(function ($value, Produit $entity) {
-                    return number_format($entity->getPrixTTC(), 2, ',', ' ') . ' €';
-                })
-                ->setFormTypeOption('disabled', true),
-            NumberField::new('delay')->setLabel('Durée de la préstation (en minutes)'),
+
+            FormField::addColumn(4,'Visuel du produit'),
             CollectionField::new('images', 'Images associées')
                 ->setEntryType(ImageType::class) // Utiliser un sous-formulaire pour chaque image
                 ->renderExpanded() // Ouvrir les sous-formulaires dans le formulaire principal
