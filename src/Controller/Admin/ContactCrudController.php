efa9ce6266cedd36d499386f0bdeade5d296ce92
Alker51 <remyrobin51@gmail.com>
2025-05-13 17:58:21 +0200

    Améliore la gestion des champs de formulaire Contact
    Ajout de champs et de mises à jour pour les informations et messages dans la vue d’administration des Contacts. Optimisation de l’affichage des dates avec formatage conditionnel et localisé en français.

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -3,14 +3,19 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Contact;
+use DateTime;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use IntlDateFormatter;
 
 
 class ContactCrudController extends AbstractCrudController
@@ -22,7 +27,11 @@ class ContactCrudController extends AbstractCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        return [
+        setlocale(LC_TIME, 'fr_FR.UTF-8', 'fra');
+
+        $fields = [
+            FormField::addColumn(4,'Informations de la demande'),
+            FormField::addFieldset(''),
             TextField::new('title', 'Sujet'),
             AssociationField::new('state', 'État de la demande')
                 ->formatValue(function ($value, $entity) {
@@ -41,8 +50,49 @@ class ContactCrudController extends AbstractCrudController
                 }),
             DateField::new('editedTime', 'Dernière modification')
                 ->setFormat('dd/MM/yyyy HH:mm')
-                ->hideOnForm(),
+                ->hideOnForm()
+                ->formatValue(
+                    function ($value, $entity) {
+                        $now = new DateTime('now', new \DateTimeZone('Europe/Paris'));
+
+                        $interval = $now->diff($entity->getEditedTime());
+
+                        if($entity->getState()->getId() === 1) {
+                            if ($interval->days <= 2) {
+                                $color = "success";
+                            } elseif ($interval->days >= 4) {
+                                $color = "danger";
+                            } else {
+                                $color = "warning";
+                            }
+                        } else {
+                            $color = "info";
+                        }
+
+                        $info = '';
+
+                        if($entity->getState()->getId() === 1){
+                            $info = ' (Ouvert depuis ' . $interval->days . ' jour' . ($interval->days > 1 ? 's' : '') . ')';
+                        }
+
+                        $formatter = new IntlDateFormatter(
+                            'fr_FR', // Locale en français
+                            IntlDateFormatter::LONG, // Format long, avec le mois en toutes lettres
+                            IntlDateFormatter::SHORT
+                        );
+
+
+                        return '<span class="badge badge-'.$color.'">' . mb_strtoupper($formatter->format($entity->getEditedTime())) . $info . '</span>';
+                    }
+                ),
+            FormField::addColumn(4,'Messages'),
+            FormField::addFieldset(''),
+            TextAreaField::new('detail', 'Message initial')->setDisabled(true)->formatValue(function ($value, $entity) {
+                return strip_tags($entity->getDetail());
+            }),
         ];
+
+        return $fields;
     }
 
     public function configureActions(Actions $actions): Actions
