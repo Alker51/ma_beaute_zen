9f537d8f2c2c8f2ede5baae5d82ee69880523949
Alker51 <remyrobin51@gmail.com>
2025-05-27 19:17:04 +0200

    Ajoute la méthode `getFullName` à l'entité User
    Ajout d'une méthode pour concaténer prénom et nom. Cela simplifie l'accès au nom complet de l'utilisateur dans l'application.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -398,4 +398,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function getFullName(): string
+    {
+        return $this->getFirstName() . ' ' . $this->getLastName();
+    }
+
 }
