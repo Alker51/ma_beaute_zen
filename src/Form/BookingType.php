4fdc970e26b6e5d96296584af58b6993f35fb5ed
Alker51 <remyrobin51@gmail.com>
2025-05-27 19:17:15 +0200

    Ajoute un champ "worker" pour les administrateurs
    Ce commit introduit un champ "worker" dans le formulaire BookingType, visible uniquement pour les administrateurs. Il utilise EntityType pour permettre la sélection d'un utilisateur ou l'option "Aucun". Une nouvelle option "is_admin" a été ajoutée pour gérer ce comportement.

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Booking;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -11,6 +13,8 @@ class BookingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $isAdmin = $options['is_admin'] ?? false;
+
         $builder
             ->add('title')
             ->add('start', null, [
@@ -30,12 +34,22 @@ class BookingType extends AbstractType
 
             ])
         ;
+        if ($isAdmin) {
+            $builder->add('worker', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'fullName',
+                'required' => false, // permet la sélection de "Aucun" (null)
+                'placeholder' => 'Aucun',
+            ]);
+        }
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Booking::class,
+            'is_admin' => false,
         ]);
     }
 }
